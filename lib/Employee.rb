7605214430975d7730e6a384db40a3f0ad0715e4
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-08-11 09:55:34 +0100

    Added outline of methods

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,49 @@
 class Employee
+    attr_reader :name, :salary #no raises for this exercise
+    attr_accessor :manager_name
+    @@all = []
+
+    def initialize(name, salary)
+        @name = name
+        @manager_name = manager_name
+        @@all << self
+    end
+
+    def name
+        @name
+    end
+
+    def salary
+        @salary
+    end
+
+    def manager_name
+        @manager_name
+    end
+
+    def self.all
+        @@all
+    end
+
+    ###methods
+
+    def self.paid_over(salary) #takes in fixnum
+
+        #return array of all employees whose salaries are OVER that amount.
+        #reduce?
+    end
+
+    def self.find_by_department(department_name) #takes in string
+        #returns FIRST employee whose manager is working in that department
+        #find - but returns a boolean
+
+    end
+
+    def tax_bracket
+        #return array of all the employees whose salaries are +/- 1000 of employee who invoked the method
+        #filter? 
+    end
+
+
 
 end
--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,52 @@
 class Manager
 
+    attr_reader :name, :age #let's just pretend they don't age for this purpose
+    attr_accessor :department, :employees
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @department = department #string
+        @age = age #Fixnum (will later require float for avg_age)
+        @employees = []
+        @@all << self
+    end
+
+    def name
+        @name
+    end
+
+    def department
+        @department
+    end
+
+    def age
+        @age
+    end
+
+    def employees
+        @employees
+    end
+
+    def self.all
+        @@all
+    end
+
+    ###methods
+
+    def hire_employee(employee_name, salary)
+        #adds that employee to the list of employees this manager oversees
+        @employees << employee_name
+    end
+
+    def self.all_departments
+        #return an array of all the department names every manager oversees
+    end
+
+    def self.average_age
+        #return float of average age of all the managers
+    end
+
+
+
 end
